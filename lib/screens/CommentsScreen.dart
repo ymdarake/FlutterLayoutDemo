427d9b14a3a3abfd9ff2fa0d85c63afbda437b8d
ymdarake <t.sakamoto.ouen@gmail.com>
2018-12-19 10:41:35 +0900

    [add] play with GestureDetector

--- lib/screens/CommentsScreen.dart
+++ lib/screens/CommentsScreen.dart
@@ -9,6 +9,7 @@ class CommentsScreen extends StatefulWidget {
     return CommentsScreenState();
   }
 }
+
 class CommentsScreenState extends State<CommentsScreen> {
   @override
   Widget build(BuildContext context) {
@@ -18,28 +19,43 @@ class CommentsScreenState extends State<CommentsScreen> {
         children: <Widget>[
           Expanded(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Text(
-                      'Comments Screen',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  'Comments Screen',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(
-                    'Kandersteg, Switzerland',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                    ),
-                  )
-                ],
-              )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              )
+            ],
+          )),
+          GestureDetector(
+            child: Icon(
+              Icons.star,
+              color: Colors.red[500],
+            ),
+            onLongPress: () {
+              print('Long press detected!');
+              _askedToLead();
+            },
+            onLongPressUp: () {
+              print('long press up detected!');
+            },
+            onHorizontalDragStart: (DragStartDetails e) {
+              print('Horizontal drag start detected!');
+              print(e.globalPosition);
+              print(e.globalPosition.dx);
+              print(e.globalPosition.dy);
+            },
           ),
           Text('41'),
         ],
@@ -59,4 +75,34 @@ class CommentsScreenState extends State<CommentsScreen> {
       })),
     );
   }
+
+  /// https://docs.flutter.io/flutter/material/SimpleDialog-class.html
+  Future<void> _askedToLead() async {
+    switch (await showDialog<int>(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: const Text('Select assignment'),
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () { Navigator.pop(context, 1); },
+                child: const Text('Treasury department'),
+              ),
+              SimpleDialogOption(
+                onPressed: () { Navigator.pop(context, 2); },
+                child: const Text('State department'),
+              ),
+            ],
+          );
+        }
+    )) {
+      case 1:
+        print(1);
+        break;
+      case 2:
+        print(2);
+        break;
+    }
+  }
+
 }
